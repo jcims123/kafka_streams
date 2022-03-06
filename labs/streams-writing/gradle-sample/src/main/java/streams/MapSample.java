7043d63d2812000714ce1f4df03c7a49f518f967
Dimitar Draganov <dimitar.draganov@gmail.com>
2022-03-06 22:57:29 +0200

    [DD] Updating the main project settings + working on the MapSamples lab

--- labs/streams-writing/gradle-sample/src/main/java/streams/MapSample.java
+++ labs/streams-writing/gradle-sample/src/main/java/streams/MapSample.java
@@ -16,25 +16,43 @@ import org.apache.kafka.streams.kstream.Produced;
 
 
 public class MapSample {
-        /*
-    This application reads data from a topic whose keys are integers and whose values are sentence strings.
-    The input values are transformed to lower-case and output to a new topic.
+    /* This application reads data from a topic whose keys are integers and whose values are sentence strings.
+       The input values are transformed to lower-case and output to a new topic.
     */
     public static void main(String[] args) {
-        System.out.println("*** Starting Map Sample Application ***");
-	
-    
+        System.out.println( "*** Starting Map Sample Application ***" );
+
         // TODO: Configure application.id and bootstrap.servers properties using the StreamsConfig class
+        Properties settings = new Properties();
+        settings.put( StreamsConfig.APPLICATION_ID_CONFIG, "map-sample-v0.1.0" );
+        settings.put( StreamsConfig.BOOTSTRAP_SERVERS_CONFIG, "localhost:19092" );
 
-        
         // TODO: Define the processor topology using the StreamsBuilder class
-
+        final Serde<String> stringSerde = Serdes.String();
+        StreamsBuilder builder = new StreamsBuilder();
+        KStream<String, String> lines = builder.stream( "lines-topic", Consumed.with( stringSerde, stringSerde ) );
+        KStream<String, String> transformed = lines.mapValues( value -> value.toLowerCase() );
+        transformed.to( "lines-lower-topic", Produced.with( stringSerde, stringSerde ) );
+        Topology topology = builder.build();
 
         // TODO: Create the KafkaStreams app
-
+        KafkaStreams streams = new KafkaStreams( topology, settings );
 
         // TODO: Add a shutdown hook for graceful termination and start the app
+        final CountDownLatch latch = new CountDownLatch( 1 );
+        Runtime.getRuntime().addShutdownHook( new Thread( () -> {
+            System.out.println( "### Stopping Map Sample Application ###" );
+            streams.close();
+            latch.countDown();
+        } ) );
 
+        try {
+            streams.start();
+            latch.await();
+        } catch (final Throwable e) {
+            System.exit( 1 );
+        }
+        System.exit( 0 );
     }
 }
 
